f41c16c591c75cd4b08329916d4d44a4dc42693e
Misha Beldishevski <mishabeldi@gmail.com>
2025-08-24 23:47:08 +0100

    Updated init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- ========= BASIC SETTINGS =========
+vim.opt.number = true -- line numbers
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
